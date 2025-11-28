24f5b350ee4aacd86e71fdc0e4375ac47b56d836
taskeen-mustafa786 <149575846+taskeen-mustafa786@users.noreply.github.com>
2025-11-28 17:51:27 +0000

    add jobresults.jsx

---
+++ frontend/src/components/JobResults.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import JobCard from "../components/JobCard";
+
+
+export default function JobResults() {
+const [jobs] = useState([]); // API will come later
+
+
+return (
+<div className="p-8">
+<h1 className="text-2xl font-bold mb-4">Job Results</h1>
+<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+{jobs.map((job, i) => <JobCard key={i} job={job} />)}
+</div>
+</div>
+);
+}
