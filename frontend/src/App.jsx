b29ae5bfa044860e2e4ecc5b0865291d498893fb
taskeen-mustafa786 <149575846+taskeen-mustafa786@users.noreply.github.com>
2025-11-28 17:42:09 +0000

    install dependencies init project and init app.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Dashboard from "./pages/Dashboard";
+import JobResults from "./pages/JobResults";
+import Profile from "./pages/Profile";
+import { Routes, Route } from "react-router-dom";
+
+
+export default function App() {
+return (
+<>
+<Navbar />
+<Routes>
+<Route path="/" element={<Dashboard />} />
+<Route path="/signup" element={<Signup />} />
+<Route path="/login" element={<Login />} />
+<Route path="/jobs" element={<JobResults />} />
+<Route path="/profile" element={<Profile />} />
+</Routes>
+</>
+);
+}
